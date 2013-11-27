23edd4abc45506b49179759206aa37e848255332
Allan Mottier <earendel.am@gmail.com>
2013-11-27 10:39:44 +0100

    added contactgroup class

---
+++ src/domain/ContactGroup.java
@@ -0,0 +1,28 @@
+package domain;
+
+public class ContactGroup {
+	private Integer groupId;
+	private String groupName;
+	
+	public ContactGroup(){
+		
+	}
+
+	public Integer getGroupId() {
+		return groupId;
+	}
+
+	public void setGroupId(Integer groupId) {
+		this.groupId = groupId;
+	}
+
+	public String getGroupName() {
+		return groupName;
+	}
+
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+	
+	
+}
