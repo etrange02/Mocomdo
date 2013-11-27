7b795cdcb2f3f546d3f25908852ddc6483ea92d5
Allan Mottier <earendel.am@gmail.com>
2013-11-27 10:16:57 +0100

    added Contact class

---
+++ src/domain/Contact.java
@@ -0,0 +1,50 @@
+package domain;
+
+public class Contact {
+	private String firstName;
+	private String lastName;
+	private String email;
+	private Integer id;
+	
+	public Contact(){
+		this.firstName="";
+		this.lastName="";
+		this.email="";
+		this.id=-1;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+	
+	
+}
